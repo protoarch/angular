61603a3bd75f809a6ae2567e759a1d0ab7e778ff
Sergey Romanchuk <rezety@gmail.com>
2022-06-15 16:18:09 +0700

    fix

--- libs/linters/jest.config.ts
+++ libs/linters/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-    displayName: 'linters',1
+    displayName: 'linters',
     preset: '../../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
