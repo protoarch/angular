a986fb50d802f689eddad32eaffb2e7ee341a406
Romanchuk Sergey <rezety@gmail.com>
2024-08-15 11:56:43 +0700

    fix: release

---
+++ tools/scripts/release/index.ts
@@ -0,0 +1,60 @@
+import logSymbols from 'log-symbols';
+import {releaseChangelog, releasePublish, releaseVersion} from 'nx/src/command-line/release';
+import * as yargs from 'yargs';
+
+(async () => {
+    const options = await yargs
+        .version(false) // don't use the default meaning of version in yargs
+        .option('version', {
+            description: 'Explicit version specifier to use, if overriding conventional commits',
+            type: 'string',
+        })
+        .option('dry-run', {
+            alias: 'd',
+            description:
+                'Whether or not to perform a dry-run of the release process, defaults to true',
+            type: 'boolean',
+            default: true,
+        })
+        .option('verbose', {
+            description: 'Whether or not to enable verbose logging, defaults to false',
+            type: 'boolean',
+            default: false,
+        })
+        .parseAsync();
+
+    const {workspaceVersion, projectsVersionData} = await releaseVersion({
+        specifier: options.version,
+        // stage package.json updates to be committed later by the changelog command
+        // stageChanges: true,
+        dryRun: options.dryRun,
+        verbose: options.verbose,
+    });
+
+    await releaseChangelog({
+        versionData: projectsVersionData,
+        version: workspaceVersion,
+        dryRun: options.dryRun,
+        verbose: options.verbose,
+    });
+    /*
+    const proc = $({
+        stdio: 'inherit',
+        all: true,
+    })`npm exec -- nx run-many -t build -p @datana-smart/*`;
+
+    await proc;
+
+
+    await gitPushWithTagsAndUpstream(options.dryRun);
+  */
+    const releaseStatus = await releasePublish({
+        dryRun: options.dryRun,
+        verbose: options.verbose,
+    });
+
+    process.exit(releaseStatus);
+})().catch(error => {
+    console.error(`\n${logSymbols.error} ${error.message}`);
+    process.exit(1);
+});
